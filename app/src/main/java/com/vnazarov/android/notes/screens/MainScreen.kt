f12b73064234a0a08bb1d03664df70abe7713dfb
Vladimir Nazarov <tooldiamond2014@gmail.com>
2022-10-20 17:34:39 +0300

    Step 3

--- app/src/main/java/com/vnazarov/android/notes/screens/MainScreen.kt
+++ app/src/main/java/com/vnazarov/android/notes/screens/MainScreen.kt
@@ -1,27 +1,42 @@
 package com.vnazarov.android.notes.screens
 
+import android.app.Application
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.vnazarov.android.notes.MainViewModel
+import com.vnazarov.android.notes.MainViewModelFactory
+import com.vnazarov.android.notes.model.Note
 import com.vnazarov.android.notes.navigation.NavRoute
 import com.vnazarov.android.notes.ui.theme.NotesTheme
 
 @Composable
 fun MainScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val mainViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+
+    val notes = mainViewModel.readTest.observeAsState(listOf()).value
+
     Scaffold(floatingActionButton = {
         FloatingActionButton(onClick = { navController.navigate(NavRoute.Add.route) }) {
             Icon(
@@ -35,63 +50,17 @@ fun MainScreen(navController: NavHostController) {
             modifier = Modifier
                 .padding(vertical = 8.dp)
         ) {
-            NoteItem(
-                title = "Note 1",
-                subtitle = "Subtitle for note 1",
-                navController = navController
-            )
-            NoteItem(
-                title = "Note 2",
-                subtitle = "Subtitle for note 2",
-                navController = navController
-            )
-            NoteItem(
-                title = "Note 3",
-                subtitle = "Subtitle for note 3",
-                navController = navController
-            )
-            NoteItem(
-                title = "Note 4",
-                subtitle = "Subtitle for note 4",
-                navController = navController
-            )
-//            NoteItem(
-//                title = "Note 5",
-//                subtitle = "Subtitle for note 5",
-//                navController = navController
-//            )
-//            NoteItem(
-//                title = "Note 6",
-//                subtitle = "Subtitle for note 6",
-//                navController = navController
-//            )
-//            NoteItem(
-//                title = "Note 7",
-//                subtitle = "Subtitle for note 7",
-//                navController = navController
-//            )
-//            NoteItem(
-//                title = "Note 8",
-//                subtitle = "Subtitle for note 8",
-//                navController = navController
-//            )
-//            NoteItem(
-//                title = "Note 9",
-//                subtitle = "Subtitle for note 9",
-//                navController = navController
-//            )
-//            NoteItem(
-//                title = "Note 10",
-//                subtitle = "Subtitle for note 10",
-//                navController = navController
-//            )
-
+            LazyColumn {
+                items(notes){ note ->
+                    NoteItem(note = note, navController = navController)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun NoteItem(title: String, subtitle: String, navController: NavHostController) {
+fun NoteItem(note: Note, navController: NavHostController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -104,8 +73,8 @@ fun NoteItem(title: String, subtitle: String, navController: NavHostController) 
             modifier = Modifier.padding(vertical = 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = title, fontSize = 24.sp, fontWeight = FontWeight.Bold)
-            Text(text = subtitle)
+            Text(text = note.title, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            Text(text = note.subtitle)
         }
     }
 }
