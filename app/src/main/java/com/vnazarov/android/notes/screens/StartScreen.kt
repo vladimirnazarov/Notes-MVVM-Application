c0e47c206e231c59c65a5df6abd10e12c4da0052
Vladimir Nazarov <tooldiamond2014@gmail.com>
2022-11-04 16:02:09 +0300

    Step 11

--- app/src/main/java/com/vnazarov/android/notes/screens/StartScreen.kt
+++ app/src/main/java/com/vnazarov/android/notes/screens/StartScreen.kt
@@ -70,7 +70,7 @@ fun StartScreen(navController: NavHostController, viewModel: MainViewModel) {
                             LOGIN = login
                             PASSWORD = password
                             viewModel.initDatabase(TYPE_FIREBASE) {
-                                DB_TYPE = TYPE_FIREBASE
+                                DB_TYPE.value = TYPE_FIREBASE
                                 navController.navigate(NavRoute.Main.route)
                             }
                         },
@@ -92,7 +92,7 @@ fun StartScreen(navController: NavHostController, viewModel: MainViewModel) {
                 Button(
                     onClick = {
                         viewModel.initDatabase(TYPE_ROOM) {
-                            DB_TYPE = TYPE_ROOM
+                            DB_TYPE.value = TYPE_ROOM
                             navController.navigate(route = NavRoute.Main.route)
                         }
                     },
--- app/src/main/java/com/vnazarov/android/notes/utils/Constants.kt
+++ app/src/main/java/com/vnazarov/android/notes/utils/Constants.kt
@@ -1,5 +1,6 @@
 package com.vnazarov.android.notes.utils
 
+import androidx.compose.runtime.mutableStateOf
 import com.vnazarov.android.notes.database.DatabaseRepository
 
 const val TYPE_DATABASE = "type_database"
@@ -10,7 +11,7 @@ const val FIREBASE_ID = "firebaseID"
 lateinit var REPOSITORY: DatabaseRepository
 lateinit var LOGIN: String
 lateinit var PASSWORD: String
-lateinit var DB_TYPE: String
+var DB_TYPE = mutableStateOf("")
 
 object Constants {
 
